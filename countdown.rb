fe5a934bb02dc06c0c984f3e37d1fee61aacaad3
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-13 14:37:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,9 @@
 #write your code here
-def countdown
- while countdown = 10 
- puts #{10} sleep(1, second)
- countdown -=1 
- return "Happy New Year!"
-end
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
